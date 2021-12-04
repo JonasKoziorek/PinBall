3a06acf585104fbcfda2aa74dcd7988f9e51eb12
Jonas <jonas.koziorek@gmail.com>
2021-12-04 21:45:02 +0100

    deleted bunch of files

--- Frame.c
+++
@@ -1,8 +0,0 @@
-#include "Frame.h"
-
-void DrawFrame( Frame *frame, SDL_Renderer *renderer ){
-        SDL_RenderDrawLine(renderer, 10, 10, 10, frame->height - 10);
-        SDL_RenderDrawLine(renderer, 10, 10, frame->width - 10, 10);
-        SDL_RenderDrawLine(renderer, 10, frame->height - 10, frame->width - 10, frame->height - 10);
-        SDL_RenderDrawLine(renderer, frame->width - 10, 10, frame->width - 10, frame->height - 10);
-}
--- Img.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "SDL2/SDL_image.h"
-
-typedef struct{
-    // path to the image
-    char sprite[50];
-    SDL_Rect rect;
-    SDL_Texture *texture;
-}Img;
